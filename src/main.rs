4808257f21d752eac252bca14394ab22afd219c8
Daisuke Yamamura <mucho613@gmail.com>
2024-06-13 10:13:45 +0900

    serial port list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Simple PPP(Point-to-Point Protocol) client
 fn main() {
-    println!("Hello, world!");
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{}", p.port_name);
+    }
 }
